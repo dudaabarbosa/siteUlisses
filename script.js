d2ffaf19f9dc84647064d1747efec8dc1994eea6
Eduarda <eduarda.santos202@etec.sp.gov.br>
2023-12-06 15:55:33 -0300

    COMIT

---
+++ script.js
@@ -0,0 +1,9 @@
+
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.apresentacao' ,{duration:3000});
+sr.reveal('#impo' ,{duration:3000});
+sr.reveal('.impo' ,{duration:3000});
+sr.reveal('#como' ,{duration:3000});
+sr.reveal('.titulo2' ,{duration:3000});
+sr.reveal('.pilares' ,{duration:3000});
